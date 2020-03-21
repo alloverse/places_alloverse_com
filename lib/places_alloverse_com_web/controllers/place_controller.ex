6782075d1d808020aba2296cb7c0465346b9a886
Emma Koszinowski <emkosz@gmail.com>
2020-03-21 13:56:10 +0100

    add place_controller

---
+++ lib/places_alloverse_com_web/controllers/place_controller.ex
@@ -0,0 +1,7 @@
+defmodule PlacesAlloverseComWeb.PlaceController do
+  use PlacesAlloverseComWeb, :controller
+
+  def index(conn, _params) do
+    render(conn, "index.html")
+  end
+end
--- lib/places_alloverse_com_web/router.ex
+++ lib/places_alloverse_com_web/router.ex
@@ -17,6 +17,7 @@ defmodule PlacesAlloverseComWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    get "/place", PlaceController, :index
   end
 
   # Other scopes may use custom stacks.
