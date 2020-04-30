1480ca39be062b78aa91e9994e834f4f56264e69
Emma Koszinowski <emkosz@gmail.com>
2020-04-30 20:30:54 +0200

    authenticate_user function

--- lib/places_alloverse_com_web/router.ex
+++ lib/places_alloverse_com_web/router.ex
@@ -26,6 +26,18 @@ defmodule PlacesAlloverseComWeb.Router do
 
   end
 
+  defp authenticate_user(conn, _) do
+    case get_session(conn, :user_id) do
+      nil ->
+        conn
+        |> Phoenix.Controller.put_flash(:error, "Login required")
+        |> Phoenix.Controller.redirect(to: "/")
+        |> halt()
+      user_id ->
+        assign(conn, :current_user, PlacesAlloverseComWeb.Accounts.get_user!(user_id))
+    end
+  end
+
   # Other scopes may use custom stacks.
   # scope "/api", PlacesAlloverseComWeb do
   #   pipe_through :api
