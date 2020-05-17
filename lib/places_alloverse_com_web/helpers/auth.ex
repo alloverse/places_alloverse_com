8dca5d91f54a0a5f310b5b7b07015df30c59813b
Emma Koszinowski <emkosz@gmail.com>
2020-05-17 21:43:13 +0200

    get_user_name helper

--- lib/places_alloverse_com_web/helpers/auth.ex
+++ lib/places_alloverse_com_web/helpers/auth.ex
@@ -5,4 +5,10 @@ defmodule PlacesAlloverseComWeb.Helpers.Auth do
     if user_id, do: !!PlacesAlloverseCom.Accounts.get_user!(user_id)
   end
 
+  def get_user_name(conn) do
+    user_id = Plug.Conn.get_session(conn, :user_id)
+    user = PlacesAlloverseCom.Accounts.get_user!(user_id)
+    user.name
+  end
+
 end
